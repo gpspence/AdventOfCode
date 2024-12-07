6624442b2d294357cb3e8b00d61c81a11264a865
gpspence <105173090+gpspence@users.noreply.github.com>
2024-12-07 19:51:55 +0000

    Work on day 3 part b - not finished

--- AdventOfCode_Day3.py
+++ AdventOfCode_Day3.py
@@ -1,7 +1,6 @@
-import numpy as np
 import re
 from aocd import submit, get_data
-from typing import List, Match
+from typing import List, Match, Iterable
 
 
 def calculate_product(match: Match) -> int:
@@ -16,16 +15,67 @@ def calculate_product(match: Match) -> int:
     return int(match.group(1)) * int(match.group(2))
 
 
+def get_new_state(
+    match: Match,
+    list_do: List,
+    list_dont: List,
+    state: bool,
+) -> bool:
+    """
+    Get the state (True or False) for the given match.
+    If do() is most recent command, return True, if don't() return False.
+    Args:(
+        match: re match object for the mul(x,y) match
+        list_do: re match iterable of all do() occurences
+        list_dont: re match iterable of all dont() occurences
+    Returns:
+        [bool]: current state, where to execute mul(x,y) command.
+    """
+    start_of_mul = match.start()
+    last_do = max([x.end() for x in list_do if (x.end() < start_of_mul)], default=-1)
+    last_dont = max([x.end() for x in list_dont if (x.end() < start_of_mul)], default=-1)
+
+    if last_do == last_dont:  # If both are -1
+        return state  # Keep previous state
+    elif last_do > last_dont:  # Make state true
+        return True
+    elif last_do < last_dont:  # Make state false
+        return False
+    else:
+        raise ValueError("No state could be determined.")
+
+
+def main(
+    list_matches: List[Match], list_do: List[Match], list_dont: List[Match]
+) -> int:
+    """
+    Given a list of matches, calculate the output of the string computation.
+    Args:
+        matches: iterable of matches returned by re.finditer()
+    Returns:
+        [int]: numeric output of the computation
+    """
+    total = 0
+    state = True
+
+    for match in list_matches:
+        state = get_new_state(match, list_do, list_dont, state)
+        if state:
+            print(match.group(0))
+            total += calculate_product(match)
+    return total
+
+
 # Part A - Multiplications from corrupted data
 # Working with test data
 test_str = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
 main_pattern = r'mul\((\d+),(\d+)\)'  # pattern to match "mul(#,#)"
-test_extracted = re.finditer(main_pattern, test_str)  # find all occurences
+test_extracted = list(re.finditer(main_pattern, test_str))  # find all occurences
 test_sum_prod = sum(list(map(calculate_product, test_extracted)))
 
-# Get the puzzle
+# Get the puzzle 
 data = get_data(day=3, year=2024)
-extracted = re.finditer(main_pattern, data)
+extracted = list(re.finditer(main_pattern, data))
 sum_prod = sum(list(map(calculate_product, extracted)))
 # submit(sum_prod, part='a', day=3, year=2024)
 
@@ -34,6 +84,13 @@ test_str_2 = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mu
 dont_pattern = r'don\'t\(\)'
 do_pattern = r'do\(\)'
 test_extracted_2 = re.finditer(main_pattern, test_str_2)
-test_extract_do = re.finditer(do_pattern, test_str_2)
-test_extract_dont = re.finditer(dont_pattern, test_extracted_2)
-state = True
+test_extract_do = list(re.finditer(do_pattern, test_str_2))
+test_extract_dont = list(re.finditer(dont_pattern, test_str_2))
+test_sum_prod_2 = main(test_extracted_2, test_extract_do, test_extract_dont)
+print(test_sum_prod_2)
+
+extract_do = list(re.finditer(do_pattern, data))
+extract_dont = list(re.finditer(dont_pattern, data))
+sum_prod_2 = main(extracted, extract_do, extract_dont)
+print(sum_prod_2)
+# submit(sum_prod_2, part='b', day=3, year=2024)
